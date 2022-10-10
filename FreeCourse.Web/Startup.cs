900bc634b15402c166aec9e9b27bd324bcda9339
mustafaguler3 <mustafa.glr@hotmail.com>
2022-10-10 18:19:48 +0300

    add usercontroller

--- FreeCourse.Web/Startup.cs
+++ FreeCourse.Web/Startup.cs
@@ -27,6 +27,8 @@ namespace FreeCourse.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+
             services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
 
             services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
